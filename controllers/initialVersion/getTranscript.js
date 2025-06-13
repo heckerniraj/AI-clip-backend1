a0f6d37903b262a03b07726f68bc1535b98a28b5
Niraj1412 <14nirajkumar@gmail.com>
2025-06-13 14:00:29 +0530

    Updating 1

--- controllers/initialVersion/getTranscript.js
+++ controllers/initialVersion/getTranscript.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PYTHON_API = process.env.PYTHON_API || 'https://clip-py-backend-1.onrender.com';
-const APPLICATION_URL = process.env.APPLICATION_URL || 'https://clip-backend-f93c.onrender.com';
+const APPLICATION_URL = process.env.APPLICATION_URL || 'https://ai-clip-backend1-1.onrender.com';
 
 // Configure global settings for Google APIs
 google.options({
